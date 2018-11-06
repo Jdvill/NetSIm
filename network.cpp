564942eccc29fdfbd9078ef3f6b9e864f1f57789
Jdvill <44576352+Jdvill@users.noreply.github.com>
2018-11-06 22:52:54 +0100

    Add files via upload

--- network.cpp
+++ network.cpp
@@ -2,13 +2,14 @@
 #include "random.h"
 #include <algorithm>
 #include <iterator>
+#include <utility>
 
-typedef std::multimap <size_t, size_t> iterator MIterator;
+typedef std::multimap <size_t, size_t>::iterator MMPIterator;
 
-void resize(const size_t& n)
+void Network::resize(const size_t& n)
 {
 	values.resize(n);
-	RandomNumbers::normal(values);
+	RNG.normal(values,0,1);
 }
 
 bool Network::add_link(const size_t& a, const size_t& b)
@@ -18,24 +19,24 @@ bool Network::add_link(const size_t& a, const size_t& b)
 	}
 	for(auto I : links)
 	{
-		if(((I->first == a)&&(I->second == b))||((I->first == b) && (I->second == a)))
+		if(((I.first == a)&&(I.second == b))||((I.first == b) && (I.second == a)))
 		{
 			return false;
 		}
 	}
-	links.insert(std::makepair(a,b));
-	links.insert(std::makepair(b,a));
+	links.insert(std::make_pair(a,b));
+	links.insert(std::make_pair(b,a));
 	return true;
 }
 
-size_t random_connect(const double& a)
+size_t Network::random_connect(const double& a)
 {
 	links.clear();
 	std::vector<size_t> vec;
-	int n;
+	unsigned int n;
 	for(size_t b(0); b<=Network::size();b++)
 	{
-		n = RandomNumbers::Poisson(a);
+		n = RNG.poisson(a);
 		for(size_t i(0); i<= Network::size(); i++)
 		{
 			if(i!=b)
@@ -43,21 +44,22 @@ size_t random_connect(const double& a)
 			vec.push_back(i);
 			}
 		}
-		RandomNumbers::shuffle(vec);
-		size_t x(0);
-		while(x<n)||(n<vec.size())
+		RNG.shuffle(vec);
+		size_t x(0), y(0);
+		while((x<n)||(n<vec.size())){
 			if(Network::addlink(b,x))
 			{
 				++x;
+				++y;
 			}else{
 				++x;
 				++n;
 		}		
 	}
-	
+	return y;
 }
 
-size_t set_values(const std::vector<double>& vec)
+size_t Network::set_values(const std::vector<double>& vec)
 {
 	size_t n(vec.size()), no(Network::size());
 	if(n>no){
@@ -73,7 +75,7 @@ size_t Network::size() const
 
 size_t Network::degree(const size_t &_n) const
 {
-	returnlinks.count(Network::value(n));
+	return links.count(Network::value(n));
 }
 
 double Network::value(const size_t &_n) const
@@ -88,11 +90,11 @@ std::vector<double> Network::sorted_values() const
 	return vals;
 }
 
-std::vector<size_t> neighbors(const size_t& a) const
+std::vector<size_t> Network::neighbors(const size_t& a) const
 {
 	std::vector<size_t> neighborslist;
-	std::pair<MIterator, MIterator> range = links.equal_range(values[a]);
-	for(MIterator it = range->first; it != range->second; it++)
+	std::pair<MMPIterator, MMPIterator> range = links.equal_range(values[a]);
+	for(MMPIterator it = range->first; it != range->second; it++)
 	{
 		neighborslist.pushback(it->second);
 	}
