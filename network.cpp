e250a6a076c5ed574792fd48a0cc99997c7e4c2f
Jdvill <44576352+Jdvill@users.noreply.github.com>
2018-11-06 13:59:15 +0100

    Add files via upload

--- network.cpp
+++ network.cpp
@@ -1,16 +1,24 @@
 #include "network.h"
+#include "random.h"
 #include <algorithm>
+#include <iterator>
+
+typedef std::multimap <size_t, size_t> iterator MIterator;
 
 void resize(const size_t& n)
 {
-	values.clear();
+	values.resize(n);
+	RandomNumbers::normal(values);
 }
 
 bool Network::add_link(const size_t& a, const size_t& b)
 {
+	if((a > Network::size())||(b > Network::size())||(a==b)){
+		return false;
+	}
 	for(auto I : links)
 	{
-		if(((I.first == a)&&(I.second == b))||((I.first == b) && (I.second == a)))
+		if(((I->first == a)&&(I->second == b))||((I->first == b) && (I->second == a)))
 		{
 			return false;
 		}
@@ -22,14 +30,32 @@ bool Network::add_link(const size_t& a, const size_t& b)
 
 size_t random_connect(const double& a)
 {
-	
+	links.clear();
+	std::vector<size_t> vec;
+	int n;
+	for(size_t b(0); b<=Network::size();b++)
+	{
+		n = RandomNumbers::Poisson(a);
+		for(size_t i(0); i<= Network::size(); i++)
+		{
+			if(i!=b)
+			{
+			vec.pushback(i);
+			}
+		}
+		RandomNumbers::shuffle(vec);
+		size_t x(1);		
+	}
 	
 }
 
 size_t set_values(const std::vector<double>& vec)
 {
-	
-	
+	size_t n(vec.size()), no(Network::size());
+	if(n>no){
+		n = no;
+	}
+	return n;
 }
 
 size_t Network::size() const
@@ -56,6 +82,11 @@ std::vector<double> Network::sorted_values() const
 
 std::vector<size_t> neighbors(const size_t& a) const
 {
-	
-	
+	std::vector<size_t> neighborslist;
+	std::pair<MIterator, MIterator> range = links.equal_range(values[a]);
+	for(MIterator it = range->first; it != range->second; it++)
+	{
+		neighborslist.pushback(it->second);
+	}
+	return neighborslist;
 }
